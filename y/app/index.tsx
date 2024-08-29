4a9917f3fb66eedf640bacc81f571cf19aa724c1
Otavio Mastrantonio <111095614+tavilom@users.noreply.github.com>
2024-08-28 21:09:21 -0300

    28/08
    commit da aula do dia 28/08/2024

--- y/app/index.tsx
+++ y/app/index.tsx
@@ -1,20 +1,35 @@
 import { Link } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import image from '../assets/images/CFDC.jpeg'; // Importa a imagem local
 
 export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            <Link href="/tela2"><Text>tela2</Text></Link>
-            <Link href="/tela3"><Text>tela3</Text></Link>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <Link href="/tela2"><Text>tela2</Text></Link>
+      <Link href="/tela3"><Text>tela3</Text></Link>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
 });
